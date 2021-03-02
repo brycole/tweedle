0998796ea608a5979bb77e4d5b4446871b58becd
Bryan Cole <brycole@gmail.com>
2021-03-02 17:56:42 +0000

    view all users via explore page now completed

--- app/Followable.php
+++ app/Followable.php
@@ -23,11 +23,12 @@ trait Followable
 
   public function toggleFollow(User $user)
   {
-    if ($this->following($user)) {
-      return $this->unfollow($user);
-    }
+    $this->follows()->toggle($user);
+    // if ($this->following($user)) {
+    //   return $this->unfollow($user);
+    // }
 
-    return $this->follow($user);
+    // return $this->follow($user);
   }
 
   public function follows()
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\TweetsController;
 use \App\Http\Controllers\ProfilesController;
 use \App\Http\Controllers\FollowsController;
+use \App\Http\Controllers\ExploreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,10 +30,13 @@ Route::middleware('auth')->group(function (){
     Route::get('/profiles/{user:username}/edit',
       [ProfilesController::class, 'edit'])->middleware('can:edit,user');
 
-    Route::patch('/profiles/{user:username}', [ProfilesController::class, 'update']);
+    Route::patch('/profiles/{user:username}', [ProfilesController::class, 'update'])->middleware('can:edit,user');
+
+    Route::get('/explore', [ExploreController::class, 'index']);
 });
 
 Route::get('/profiles/{user:username}', [ProfilesController::class, 'show'])->name('profile');
+
 
 Auth::routes();
 
